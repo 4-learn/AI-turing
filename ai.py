27fb591ebf17823c6defb1ea2c537c14d569ce15
yillkid <yillkid@gmail.com>
2024-05-20 10:42:52 +0000

    langchain-agent-custom

--- ai.py
+++ ai.py
@@ -1,31 +1,42 @@
 import requests
 import json
-import os
+from langchain.agents import tool, load_tools, initialize_agent, AgentType
+from langchain_openai import OpenAI
 
-def chat(text):
-    payload = json.dumps({
-      "role": os.getenv("BOT_NAME"),
-      "type": "RAG",
-      "tools": [
-        {
-          "tool": "agent",
-          "load_tools": "custom"
-        }
-      ],
-      "// model": "mistralai/Mistral-7B-Instruct-v0.2",
-      "// format": "html",
-      "message": text
-    })
+@tool
+def device_info(text: str) -> str:
+    """Return device's status."""
+    url = "http://211.21.113.190:8155/api/states/switch.mqtt_ntnu_1_2_sw_2"
+
+    payload = ""
     headers = {
-      'Content-Type': 'application/json'
+        'Content-type': 'application/json ',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI4YWM0MDEzODIwNDU0MDE0ODdjNzIwZTc2ZDBmYzdjYSIsImlhdCI6MTY5ODgwNzExNSwiZXhwIjoyMDE0MTY3MTE1fQ.7KaCwPUcjAr_zne04qili2fwQO1QoWTPzsmV1v_LLIc'
     }
 
-    response = requests.request("POST", os.getenv("API_ENDPOINT_LLMTWINS") + "/prompt", headers=headers, data=payload)
+    response = requests.request("GET", url, headers = headers, data = payload)
 
-    # Convert response to JSON
+    return json.dumps(response.text)
+
+def chat(text):
+    llm = OpenAI(temperature=0)
+
+    # Load the tools
+    tools = load_tools([], llm=llm)
+
+    # Initialize the agent with the specific tool
+    agent = initialize_agent(
+        tools + [device_info],
+        llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        handle_parsing_errors=True,
+        max_execution_time=30,
+        verbose=False
+    )
+
     try:
-        response = response.json()
-        return response["message"]
+        # Ensure to pass a parameter when calling the agent
+        result = agent({"input": text})
+        return result['output']
     except Exception as e:
         print(str(e))
-        return os.getenv("BOT_NAME") + " 壞掉了，趕快請人類來修理: " + str(e)
